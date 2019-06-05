2005a9282eac38e544ac9b9624a887b1e2f8b6ef
Marco Crippa <m.crippa84@gmail.com>
2019-06-06 00:17:37 +0200

    scrub

--- Custom HTML5 Video Player/script.js
+++ Custom HTML5 Video Player/script.js
@@ -7,7 +7,7 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 function togglePlay() {
-    this.paused ? video.play() : video.pause();
+    video.paused ? video.play() : video.pause();
 }
 
 function updateButton() {
@@ -22,9 +22,28 @@ function handleRangeUpdate() {
     video[this.name] = this.value;
 }
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(slider => slider.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mousemove', () => mousedown = false);
+
