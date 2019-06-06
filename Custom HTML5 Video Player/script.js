0ee434e10b7f876ccee53fcdc5fc4dbf9029a253
Marco Crippa <m.crippa84@gmail.com>
2019-06-06 19:42:45 +0200

    time tracker and fullscreen mode

--- Custom HTML5 Video Player/script.js
+++ Custom HTML5 Video Player/script.js
@@ -5,6 +5,8 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const time = player.querySelector('.progress__time');
+const fullScreenButton = document.querySelector('.fullscreen');
 
 function togglePlay() {
     video.paused ? video.play() : video.pause();
@@ -32,10 +34,25 @@ function scrub(e) {
     video.currentTime = scrubTime;
 }
 
+function formatTime(seconds) {
+    let minutes = Math.floor(seconds / 60);
+    minutes = (minutes >= 10) ? minutes : "0" + minutes;
+    seconds = Math.floor(seconds % 60);
+    seconds = (seconds >= 10) ? seconds : "0" + seconds;
+    return minutes + ":" + seconds;
+};
+
+function updateTime() {
+    console.log(video.duration);
+    time.textContent = `${formatTime(video.currentTime)} / ${formatTime(video.duration)}`;
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
+video.addEventListener('timeupdate', updateTime);
+video.addEventListener('loadedmetadata', updateTime);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(slider => slider.addEventListener('change', handleRangeUpdate));
@@ -46,4 +63,6 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mousemove', () => mousedown = false);
-
+fullScreenButton.addEventListener('click', () => {
+    video.requestFullscreen();
+});
