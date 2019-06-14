a3a5c9e25f7e83bea5788942f2177d844f85495f
Marco Crippa <m.crippa84@gmail.com>
2019-06-14 18:53:12 +0200

    check / clear functionalities

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,6 +1,9 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
+const checkAllButton = document.querySelector('#check-all');
+const uncheckAllButton = document.querySelector('#uncheck-all');
+const clearButton = document.querySelector('#clear');
 
 function addItem(e) {
   e.preventDefault();
@@ -37,9 +40,27 @@ function toggleDone(e) {
   populateList(items, itemsList);
 }
 
+function checkAll() {
+  items.forEach(item => (item.done = true));
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function uncheckAll() {
+  items.forEach(item => (item.done = false));
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function clearAll() {
+  localStorage.clear();
+  populateList(undefined, itemsList);
+}
+
 addItems.addEventListener('submit', addItem);
 itemsList.addEventListener('click', toggleDone);
+checkAllButton.addEventListener('click', checkAll);
+uncheckAllButton.addEventListener('click', uncheckAll);
+clearButton.addEventListener('click', clearAll);
 
 populateList(items, itemsList);
-
-// add buttons: clear, checkAll, uncheckAll
